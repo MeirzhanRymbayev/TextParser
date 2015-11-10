0e29a2392c63d35f90a80c09ebe6d6e1a6aeb63a
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2015-11-11 00:12:26 +0600

    log4j.xml file was configured. Three appenders(console, io, fileWithRootLogs) was created.

--- src/main/java/com/epam/mrymbayev/Main.java
+++ src/main/java/com/epam/mrymbayev/Main.java
@@ -7,12 +7,16 @@ import com.epam.mrymbayev.parser.SimpleParser;
 import com.epam.mrymbayev.io.MTextReader;
 import com.epam.mrymbayev.parser.exception.ParseException;
 import com.epam.mrymbayev.parser.exception.PropertyFilePathException;
+import org.apache.log4j.Logger;
 
 /**
  * Hello world!
  */
 public class Main {
-    public static void main(String[] args) throws  ReadingException, PropertyFilePathException, ParseException {
+
+    private static final Logger log = Logger.getLogger(Main.class);
+
+        public static void main(String[] args) throws  ReadingException, PropertyFilePathException, ParseException {
 
         MTextReader reader = new MTextReader();
         String s = reader.getFullText("src/main/resources/File1.txt");
@@ -20,8 +24,8 @@ public class Main {
         Text parsedText = simpleParser.parse(s);
 
 
-
         String text = parsedText.toSourceString();
-        System.out.println("Is two texts equal? Answer: [" + s.equals(text) + "]");
+        log.info("Start to compare TWO text instances");
+
     }
 }
--- src/main/java/com/epam/mrymbayev/io/MTextReader.java
+++ src/main/java/com/epam/mrymbayev/io/MTextReader.java
@@ -1,6 +1,7 @@
 package com.epam.mrymbayev.io;
 
 import com.epam.mrymbayev.io.exception.ReadingException;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,22 +11,28 @@ import java.util.Scanner;
  * Created by Meir on 09.11.2015.
  */
 public class MTextReader implements Reader {
+    Logger ioLog = Logger.getLogger(MTextReader.class);
+
     @Override
-    public String getFullText(String path) throws ReadingException {
-        System.out.println("Reading file:" + path +"...");
+    public String getFullText(final String path) throws ReadingException {
+        ioLog.info("Try to find \"" + path + "\" file to read.");
+        //System.out.println("Reading file \"" + path +"\"...");
         StringBuilder fullText = new StringBuilder();
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(path)).useDelimiter("\\Z");
+            ioLog.info("File found.");
+            ioLog.info("Reading file...");
             while(scanner.hasNextLine()){
                 fullText.append(scanner.nextLine()).append("\n");
             }
         } catch (FileNotFoundException e) {
-            throw new ReadingException("File " + path + "not found. Please, use correct path to file.");
+            ioLog.error("File " + path + " not found. Please, check path to file.");
+            throw new ReadingException("File " + path + " found. Please, use correct path to file.");
             } finally {
             scanner.close();
         }
-        System.out.println("Reading finished...");
+        ioLog.info("Reading finish success.");
         return fullText.toString();
 
     }
